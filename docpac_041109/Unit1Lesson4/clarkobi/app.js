0877e1c30275db2ec3e6ee1242b6bd036ef2c5ee
Obi174 <28OClark@live.ytech.edu>
2026-09-11 14:56:14 -0400

    updating U1L4

--- docpac_041109/Unit1Lesson4/clarkobi/app.js
+++ docpac_041109/Unit1Lesson4/clarkobi/app.js
@@ -9,10 +9,27 @@ dotenv.config()
 const fs = require('fs')
 
 const http = require('http');
+const { text } = require('stream/consumers');
 const server = http.createServer((req, res) => {
-    res.writeHead(200,'text/plain','Hello')
-    res.end('Hello')
-});
+    if (req.url === '/form') {
+        if (req.method === 'POST') {}
+        //if post request
+        //read form body data
+    } else {
+
+        fs.readFile('pages/form.html', 'utf8', (err, data) => {
+            if (err) {
+
+                res.writeHead(500, { 'Content-Type': 'text/plain' })
+                res.end('Error reading file')
+            } else {
+                res.writeHead(200, { 'content-type': 'text/html' })
+                res.end(data)
+            }
+        })
+    }
+}
+);
 
 server.listen(process.env.PORT, () => {
     console.log(process.env.APP)
