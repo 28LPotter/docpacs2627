896a83bc67be8b8b1b79ad8f1546d667a0d80cc5
28LPotter <28LPotter@live.ytech.edu>
2026-09-15 14:56:04 -0400

    worked on u1l5, post is done, query is unfinished

--- docpac_051809/Unit1Lesson5/PotterLogan/index.js
+++ docpac_051809/Unit1Lesson5/PotterLogan/index.js
@@ -1,38 +1,68 @@
 require('dotenv').config()
 fs = require('fs')
 http = require('http')
-
+url = require('url')
 const PORT = process.env.PORT
 
 
 
 
 server = http.createServer((req, res) => {
-    
+
+    let parsed = url.parse(req.url)
 
     if (req.url == "/form") {
-        fs.readFile("pages/form.html", "UTF8", (error, data) => {
-            
-            if (error) {
-                res.writeHead(500, { 'Content-Type': 'text/html' })
-                res.end("uh oh")
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' })
-                res.end(data)
-            }
-        })
-    } else if(req.url == "/"){
+
+        if (req.method == "GET") {
+            fs.readFile("pages/form.html", "UTF8", (error, data) => {
+
+                if (error) {
+                    res.writeHead(500, { 'Content-Type': 'text/html' })
+                    res.end("uh oh")
+                } else {
+                    res.writeHead(200, { 'Content-Type': 'text/html' })
+                    res.end(data)
+                }
+            })
+        } else if (req.method == "POST") {
+            let body
+            req.on("data", (chunk) => {
+                body += "&"
+                body += chunk
+
+            })
+            req.on("end", () => {
+                console.log(body.toString("UTF8"))
+                console.log(body.slice(10))
+                body = new URLSearchParams(body)
+                console.log(body.get("name"))
+
+                res.writeHead(200, { 'Content-Type': 'text/plain' })
+                res.end("your name is " + body.get("name"))
+            })
+        }
+    } else if (req.url == "/") {
         res.writeHead(200, { 'Content-Type': 'text/plain' })
         res.end("Hello World")
+    } else if (parsed.pathname == "/query") {
+        if (parsed.pathname.message) {
+            res.writeHead(200, { 'Content-Type': 'text/plain' })
+            res.end(parsed.query.message)
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/plain' })
+            res.end("formatting issue")
+            console.log(parsed.query + " " + parsed.query.message)
+        }
+
     } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' })
         res.end("URL not found!")
     }
     console.log("request for " + req.url)
-    //res.end(data)
+    //res.end
 });
 
 server.listen(PORT, 'localhost', () => {
     console.log("running at " + PORT)
-    
+
 })
