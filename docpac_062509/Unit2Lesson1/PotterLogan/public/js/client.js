17d5eadebb1f06c57da710ca05fcf40900fd7253
28LPotter <28LPotter@live.ytech.edu>
2026-09-22 09:30:31 -0400

    Unit 2 lesson 1 complete

--- docpac_062509/Unit2Lesson1/PotterLogan/public/js/client.js
+++ docpac_062509/Unit2Lesson1/PotterLogan/public/js/client.js
@@ -1,9 +1,1 @@
-require('dotenv').config()
-fs = require('fs')
-http = require('http')
-url = require('url')
-express = require('express')
-app = express();
-const PORT = process.env.PORT
-
-app.use(express.static(path.join(__dirname, 'public')))
+console.log("hell")
