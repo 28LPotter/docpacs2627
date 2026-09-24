3a82f08fdd50d2f6adba81e027b74a7291108886
28LPotter <28LPotter@live.ytech.edu>
2026-09-24 10:11:13 -0400

    began touch controls (hell)

---
+++ docpac_062509/Touch Controls/PotterLogan/touch-controls/touch.js
@@ -0,0 +1,54 @@
+
+
+
+touchArea = document.getElementById("toucharea")
+object = document.getElementById("player")
+touchstatus = document.getElementById("touchStatus")
+
+touchActive = false
+startX = 50
+startY = 50
+currentX = 50
+currentY = 50
+deltaX = 0
+deltaY = 0
+
+areaRectangle = touchArea.getBoundingClientRect()
+
+SWIPE_DISTANCE = 400
+TAP_TOLERANCE = 50
+
+addEventListener("touchstart", (event) => {
+
+    if (event.touches[0] != null) {
+        startX = event.touches[0].clientX
+        startY = event.touches[0].clientY
+        currentX = event.touches[0].clientX
+        currentY = event.touches[0].clientY
+
+        console.log("X", currentX)
+        console.log("Y", currentY)
+
+
+        localX = event.touches[0].clientX - areaRectangle.left
+        localY = event.touches[0].clientY - areaRectangle.top
+
+        console.log(localX)
+
+    }
+
+
+})
+
+
+addEventListener("touchmove", (event) => {
+
+    currentX = event.touches[0].clientX
+    currentY = event.touches[0].clientY
+    deltaX = currentX - startX
+    deltaY = currentY - startY
+
+    object.style.top = `${deltaY}px`
+
+
+})
